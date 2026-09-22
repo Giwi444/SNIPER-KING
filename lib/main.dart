5e49fd1a3a85ee2ac8b23e6a83a6b6bd2a880763
Giwi444 <tiktoktech2024@gmail.com>
2026-09-22 12:15:42 +0700

    Change app title from 'Liquidity App' to 'Sniper App'

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Liquidity App',
+      title: 'Sniper App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: const Color(0xFF0A0A0C),
